8bb8818f5f3c9d32729b0544f74244f039f32f36
Tommy Dang <theventus@gmail.com>
2017-09-08 23:53:58 -0700

    Extension now keeps track of the hostname ever 5s! Removed redundant hostname code in popup.js

---
+++ background.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	showHostName();
+});
+
+function showHostName () {
+	setInterval(function(){
+	chrome.tabs.query({
+		"active": true,
+		"lastFocusedWindow": true
+	}, function(tabArray) {
+		var url = new URL(tabArray[0].url);
+		var hostname = url.hostname;
+		alert("The hostname is: " + hostname);
+	});
+}, 5000);
+}
--- popup.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	chrome.tabs.executeScript({
-		code: 'console.log("The hostname is: " + window.location.hostname)'
-	});
-  });
